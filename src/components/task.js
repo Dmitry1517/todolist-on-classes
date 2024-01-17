221412a6da32deff401bdeebed9833d70a72cad4
Dmitry Ivannikov <postbox404@gmail.com>
2024-01-17 17:27:18 +1000

    install date-fns

--- src/components/task.js
+++ src/components/task.js
@@ -10,7 +10,7 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-// import { formatDistanceToNow } from 'https://unpkg.com/date-fns/formatDistanceToNow.mjs';
+import { formatDistanceToNow } from "date-fns";
 
 export default class Task extends Component {
   state = {
